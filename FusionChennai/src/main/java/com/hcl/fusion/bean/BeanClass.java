f2b73777a7098cc6391bf78e89d1f8261e3ccbea
THANGRAJ NAMBI <THANGRAJ NAMBI@192.168.2.3>
2016-10-01 13:36:09 +0530

    iam deleting sop

--- FusionChennai/src/main/java/com/hcl/fusion/bean/BeanClass.java
+++ FusionChennai/src/main/java/com/hcl/fusion/bean/BeanClass.java
@@ -1,38 +1,36 @@
 package com.hcl.fusion.bean;
 
-
-public class BeanClass{
+public class BeanClass {
 
 	private int Id;
 
 	private String userName;
 
 	private String passWord;
-	
+
 	public int getId() {
 		return Id;
 	}
-	
+
 	public void setId(int id) {
 		Id = id;
 	}
-	
+
 	public String getUserName() {
-		System.out.println("this is the method getUserName()");
 		return userName;
 	}
-	
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	
+
 	public String getPassWord() {
 		System.out.println("jska");
 		return passWord;
 	}
+
 	public void setPassWord(String passWord) {
 		this.passWord = passWord;
 	}
-
 
 }
